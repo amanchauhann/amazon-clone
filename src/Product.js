2c93816c8918eb9411302d899866b342d08c72cc
Aman Chauhan <amangotpro@gmail.com>
2022-02-07 23:24:21 +0530

    Initiated: installed router-dom

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,6 @@
 import './Product.css'
 
-function Product({ id, title, image, price, rating}) {
+function Product({ key, title, image, price, rating}) {
     return (
         <div className='product'>
         <div className='product__info'>
