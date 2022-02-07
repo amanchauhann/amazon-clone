d46422fef1081955e50558e4895cc55fde1fee3f
Aman Chauhan <amangotpro@gmail.com>
2022-02-07 23:24:40 +0530

    Initiated: installed router-dom

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={<Header />} />
+          <Route path="/" element={[<Header />, <Home />]} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
