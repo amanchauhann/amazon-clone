0d8b912872c97e7347ab304c61c4c4a14fc0f4af
Aman Chauhan <amangotpro@gmail.com>
2022-02-07 15:01:33 +0530

    Refactor: Cleaned up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDtCJNCjobqYdcprHnuIs8db9UXDbzaHqM",
+    authDomain: "fir-445cd.firebaseapp.com",
+    projectId: "fir-445cd",
+    storageBucket: "fir-445cd.appspot.com",
+    messagingSenderId: "4177973496",
+    appId: "1:4177973496:web:a051ce307bd57079cd2b0d",
+    measurementId: "G-T0F7D9XPL2"
+};
