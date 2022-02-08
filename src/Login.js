ab42eb4ddeee501e16a2896c2406470f7031688a
Aman Chauhan <amangotpro@gmail.com>
2022-02-08 22:13:48 +0530

    Initiate: Login

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import './Login.css'
+
+function Login() {
+    return (
+        <div className='login'>
+            <Link to='/'>
+            <img className='login__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' />
+            </Link>
+
+            <div className='login__container'>
+                <h1>Sign in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type={'text'} />
+
+                    <h5>Password</h5>
+                    <input type={'password'} />
+
+                    <button>Sign in</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login;
