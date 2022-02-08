7fbdc72a38e493e4dfa839a7c56ce56e95c038ee
Aman Chauhan <amangotpro@gmail.com>
2022-02-08 12:40:03 +0530

    Style: Subtotal stying

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,9 +1,28 @@
 import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format';
 
 function Subtotal() {
     return (
         <div className='subtotal'>
+            <CurrencyFormat
+            renderText={(value) => (
+                <>
+                    <p>
+                        Subtotal (0 item): <strong>0</strong>
+                    </p>
+                    <small className='subtotal__gift'>
+                        <input type={"checkbox"} />This contains a gift
+                    </small>
+                </>
+            )}
+            decimalScale={2}
+            value={0}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+            />
 
+            <button>Proceed to checkout</button>
         </div>
     )
 }
