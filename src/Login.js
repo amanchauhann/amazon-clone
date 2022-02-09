7c0d43b79435bd23c7fe9946101f3cd4db63fcb1
Aman Chauhan <amangotpro@gmail.com>
2022-02-09 11:57:52 +0530

    Import: useHistory

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { auth } from './firebase';
 import './Login.css'
 
 function Login() {
+    const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
